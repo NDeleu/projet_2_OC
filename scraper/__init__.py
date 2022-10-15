7c3e774498ece7234520e8af116ec1ebf9101cec
Nicolas Deleu <n.deleu@hotmail.fr>
2022-10-16 00:58:38 +0200

    add Sphinx in requirements.txt as module for creat doc

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,6 +1,8 @@
 from .runscrap import RunScrap
 
 """
+Projet_2_OC : Utilisez les bases de Python
+
 Ceci est une application permettant d'extraire, de transformer et de charger des données (pipeline ETL) 
 Les données extraites seront proposées en fichier csv et image jpg.
 Les informations sont issues du site Book to Scrape, et les données extraites sont explicitées dans le README.txt
