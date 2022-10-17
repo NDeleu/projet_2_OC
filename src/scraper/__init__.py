039ff3f8b04e69762bb0f597cc0359269a90d9d2
Nicolas Deleu <n.deleu@hotmail.fr>
2022-10-17 10:59:08 +0200

    maj README.txt => README.md

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -5,5 +5,5 @@ Projet_2_OC : Utilisez les bases de Python
 
 Ceci est une application permettant d'extraire, de transformer et de charger des données (pipeline ETL) 
 Les données extraites seront proposées en fichier csv et image jpg.
-Les informations sont issues du site Book to Scrape, et les données extraites sont explicitées dans le README.txt
+Les informations sont issues du site Book to Scrape, et les données extraites sont explicitées dans le README.md
 """
